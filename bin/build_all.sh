10027023c709795bc3640d3197cc32e0a5c6f8b6
Kevin O'Donnell <kevin@blockchaintp.com>
2019-10-06 12:28:01 -0400

    Add templates for sextant
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

---
+++ bin/build_all.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+TARGET_S3=s3://scealiontach/charts
+KEY=${KEY:-kevin@blockchaintp.com}
+KEY_RING=${KEY_RING:-~/.gnupg/secring.gpg}
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+src_dir=${DIR}/..
+
+pushd $src_dir
+
+for chart in `ls |grep -v "dist\|bin\|yaml"`; do
+  set -x
+  helm dep up $chart
+  helm package --sign --key $KEY --keyring $KEY_RING -d $src_dir/dist ./$chart 
+done
+
+pushd $src_dir/dist
+helm repo index ./
+aws s3 sync . s3://scealiontach/charts
+popd
+
+popd
