7f9a3a773e46d683b21138bd3ccc1a6e674836c0
Kevin O'Donnell <kevin@blockchaintp.com>
2020-08-25 21:47:38 -0400

    fix(daml-on-besu): add persistence options to form
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- charts/daml-on-besu/sextant/form.js
+++ charts/daml-on-besu/sextant/form.js
@@ -68,6 +68,82 @@ const form = [
     },
   },
 
+  {
+    id: 'besu.persistence.enabled',
+    title: 'Besu Persistence',
+    helperText:
+      'If enabled data will be stored on PersistentVolumeClaims ',
+    component: 'radio',
+    default: false,
+    dataType: 'boolean',
+    editable: {
+      new: true,
+    },
+    row: true,
+    options: options.activated,
+    validate: {
+      type: 'string',
+      methods: [['required', 'Required']],
+    },
+  },
+  {
+    id: 'besu.persistence.storageClass',
+    title: 'Besu StorageClass',
+    helperText: 'The name of the StorageClass for the PersistentVolumeClaims',
+    component: 'text',
+    default: null,
+    editable: {
+      new: true,
+    },
+    validate: {
+      type: 'string',
+      methods: [
+        [
+          'matches', [`^[a-z]([-a-z0-9]*[a-z0-9])*$`],
+          'a DNS-1123 label must consist of lower case alphanumeric characters or \'-\', and must start and end with an alphanumeric character'
+        ]
+      ],
+    },
+  },
+
+  {
+    id: 'postgres.persistence.enabled',
+    title: 'Postgres Persistence',
+    helperText:
+      'If enabled data will be stored on PersistentVolumeClaims ',
+    component: 'radio',
+    default: false,
+    dataType: 'boolean',
+    editable: {
+      new: true,
+    },
+    row: true,
+    options: options.activated,
+    validate: {
+      type: 'string',
+      methods: [['required', 'Required']],
+    },
+  },
+  {
+    id: 'postgres.persistence.storageClass',
+    title: 'Postgres StorageClass',
+    helperText: 'The name of the StorageClass for the PersistentVolumeClaims',
+    component: 'text',
+    default: null,
+    editable: {
+      new: true,
+    },
+    validate: {
+      type: 'string',
+      methods: [
+        [
+          'matches', [`^[a-z]([-a-z0-9]*[a-z0-9])*$`],
+          'a DNS-1123 label must consist of lower case alphanumeric characters or \'-\', and must start and end with an alphanumeric character'
+        ]
+      ],
+    },
+  },
+
   'Image Pull Secrets',
 
   {
