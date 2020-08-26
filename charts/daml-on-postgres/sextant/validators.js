cfbfefb6d5f050a192bc1cd93404244c9f80b7ed
Kevin O'Donnell <kevin@blockchaintp.com>
2020-08-25 23:39:11 -0400

    fix(daml-on-postgres): allow zero length for password fields
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- charts/daml-on-postgres/sextant/validators.js
+++ charts/daml-on-postgres/sextant/validators.js
@@ -3,7 +3,7 @@ const dns1123 = [
   'a DNS-1123 label must consist of lower case alphanumeric characters or \'-\', and must start and end with an alphanumeric character',
 ]
 
-const password = ['matches', '^\\S+$', 'Cannot contain spaces']
+const password = ['matches', '^\\S*$', 'Cannot contain spaces']
 
 module.exports = {
   dns1123,
