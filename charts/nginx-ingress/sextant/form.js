18e9ade71d43ca96173ae1a69126fef57e9db967
Kevin O'Donnell <kevin@blockchaintp.com>
2020-08-26 09:21:57 -0400

    fix(nginx-ingress): correct sextant/form.js
    Signed-off-by: Kevin O'Donnell <kevin@blockchaintp.com>

--- charts/nginx-ingress/sextant/form.js
+++ charts/nginx-ingress/sextant/form.js
@@ -4,9 +4,7 @@
  * License: Product
  */
 
-const options = require('./options')
 const common = require('./common')
-const validators = require('./validators')
 
 const form = [
 
@@ -15,41 +13,6 @@ const form = [
   common.deploymentFields,
 
   common.affinityEnabled,
-
-  {
-    id: 'besu.persistence.enabled',
-    title: 'Besu Persistence',
-    helperText:
-      'If enabled data will be stored on PersistentVolumeClaims ',
-    component: 'radio',
-    default: false,
-    dataType: 'boolean',
-    editable: {
-      new: true,
-    },
-    row: true,
-    options: options.activated,
-    validate: {
-      type: 'string',
-      methods: [['required', 'Required']],
-    },
-  },
-  {
-    id: 'besu.persistence.storageClass',
-    title: 'Besu StorageClass',
-    helperText: 'The name of the StorageClass for the PersistentVolumeClaims',
-    component: 'text',
-    default: null,
-    editable: {
-      new: true,
-    },
-    validate: {
-      type: 'string',
-      methods: [
-        validators.dns1123,
-      ],
-    },
-  },
 
   'Image Pull Secrets',
 
