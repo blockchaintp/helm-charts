6abcfe6912f7eb3da1dbc1ee69ea0eadbaae6811
Mikeala Sheldt <mikaela@blockchaintp.com>
2020-09-14 13:07:52 -0400

    feat(charts): add daml on qldb chart
    sxt-430
    
    Signed-off-by: Mikeala Sheldt <mikaela@blockchaintp.com>

---
+++ charts/daml-on-qldb/sextant/summary.js
@@ -0,0 +1,41 @@
+/*
+ * Copyright © 2020 Blockchain Technology Partners Limited All Rights Reserved
+ *
+ * License: Product
+ */
+
+const secretValue = (daml) => {
+  if (daml.postgres) {
+    return daml.postgres.secret
+  }
+  return null
+}
+
+const summary = (values) => {
+  const {
+    deployment,
+    daml,
+  } = values
+
+  return [{
+    title: 'Deployment Name',
+    value: deployment.name,
+  }, {
+    title: 'Namespace',
+    value: deployment.namespace,
+  }, {
+    title: 'DAML DB Connection Secret',
+    value: secretValue(daml),
+  }, {
+    title: 'DAML Ledger ID',
+    value: daml.ledgerId,
+  }, {
+    title: 'QLDB Ledger Name',
+    value: daml.ledgerId,
+  }, {
+    title: 'DAML GRPC Port',
+    value: '39000',
+  }]
+}
+
+module.exports = summary
